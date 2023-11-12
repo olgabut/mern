9566ffa1cf8b5aebed72f6daaadf409ffd5c2c7e
solsarok <solsarok@gmail.com>
2023-11-12 17:02:36 +0100

    feat: client login logout

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,14 +1,21 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+import { AuthContext } from '../context/AuthContext';
 import { useHttp } from '../hooks/http.hook';
+import { useMessage } from '../hooks/message.hook';
 
 export const AuthPage = () => {
-    const {loading, error, request} = useHttp()
+    const auth = useContext(AuthContext)
+    const message = useMessage()
+    const {loading, error, request, clearError} = useHttp()
     const [form, setForm] = useState({
         email: '',
         password: ''
     });
 
-    useEffect(() => {}, [error])
+    useEffect(() => {
+        message(error)
+        clearError()
+    }, [error, message, clearError])
 
     const changeHandler = (event) => {
         setForm({...form, [event.target.name]: event.target.value})
@@ -17,7 +24,14 @@ export const AuthPage = () => {
     const registerHandler = async () => {
         try {
             const data = await request('/api/auth/register', 'POST', {...form})
-            console.log('data', data)
+            message(data.message)
+        } catch (e) {}
+    }
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form})
+            auth.login(data.token, data.userId)
         } catch (e) {}
     }
 
@@ -60,6 +74,7 @@ export const AuthPage = () => {
                     <button 
                         className="btn yellow darken-4" 
                         style={{marginRight: 10}}
+                        onClick={loginHandler}
                         disabled={loading}
                     >
                         Login
