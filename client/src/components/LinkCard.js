c23ec56b15e8ddb5fcc8cc9e976e4b01c5629a3d
solsarok <solsarok@gmail.com>
2023-11-16 14:01:33 +0100

    feat: links list

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -3,11 +3,11 @@ import React from 'react'
 export const LinkCard = ({link}) => {
     return (
         <>
-        <h2>Link</h2>
-        <p>Your link: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
-        <p>From: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
-        <p>Count clicks: {link.clicks}</p>
-        <p>Created date: {new Date(link.date).toLocaleDateString()}</p>
+            <h2>Link</h2>
+            <p>Your link: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
+            <p>From: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
+            <p>Count clicks: {link.clicks}</p>
+            <p>Created date: {new Date(link.date).toLocaleDateString()}</p>
         </>
     )
 }
