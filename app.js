8e9bd09173a1ce46308933642de5b13b035ed0fb
solsarok <solsarok@gmail.com>
2023-11-23 14:22:10 +0100

    feat: app.use

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use('/t', require('./routes/redirect.routes'))
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    //app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.use(express.static(path.join(__dirname, 'client', 'build')))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
